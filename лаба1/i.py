d89c0a1a173af6f378df694aabc213a8f39a0654
amelinnikolay07-ops <amelinnikolay07@mail.ru>
2026-09-16 08:07:26 +0300

    Update i.py

--- лаба1/i.py
+++ лаба1/i.py
@@ -1,32 +1,14 @@
-s1 = input().strip()
-s2 = input().strip()
+from collections import Counter
 
-if len(s1) != len(s2):
+wrd1 = input()
+wrd2 = input()
+
+
+if len(wrd1) != len(wrd2):
     print("NO")
 else:
-    counts = {}
 
-    for char in s1:
-        if char in counts:
-            counts[char] += 1
-        else:
-            counts[char] = 1
-
-    for char in s2:
-        if char in counts:
-            counts[char] -= 1
-        else:
-
-            print("NO")
-            exit()
-
-    is_anagram = True
-    for count in counts.values():
-        if count != 0:
-            is_anagram = False
-            break
-
-    if is_anagram:
+    if Counter(wrd1) == Counter(wrd2):
         print("YES")
     else:
         print("NO")
