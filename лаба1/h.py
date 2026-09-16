2781bbd46bc39ace2f4b8535e2b7668db6798866
amelinnikolay07-ops <amelinnikolay07@mail.ru>
2026-09-16 08:05:27 +0300

    Update h.py

--- лаба1/h.py
+++ лаба1/h.py
@@ -1,32 +1,19 @@
 from dataclasses import dataclass
-import sys
-from math import sqrt
+
 
 @dataclass
 class Point:
     x: int
     y: int
 
-    def distance(self):
-        return sqrt(self.x**2 + self.y**2)
 
 n = int(input())
-if n > 100:
-    sys.exit()
+Points = []
 
-arr = []
 for i in range(n):
     x, y = map(int, input().split())
-    arr.append(Point(x, y))
 
-for i in range(n):
-    minimum = arr[i].distance()
-    for j in range(i+1, n):
-        if arr[j].distance() < minimum:
-            minimum = arr[j].distance()
-            arr[i], arr[j] = arr[j], arr[i]
-
-
-for i in range(n):
-    print(arr[i].x, arr[i].y)
-    
+    Points.append(Point(x, y))
+Points.sort(key=lambda p: p.x**2 + p.y**2)
+for p in Points:
+    print(p.x, p.y)
