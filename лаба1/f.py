8d975b8e8d6955b4d6b189f74e4fabca1828bee2
amelinnikolay07-ops <amelinnikolay07@mail.ru>
2026-09-16 08:02:09 +0300

    Update f.py

--- лаба1/f.py
+++ лаба1/f.py
@@ -1,43 +1,19 @@
-import sys
-import random
+from random import *
 
-def quickSort(arr, left, right):
-    if left < right:
-        pivot = separation(arr, left, right)
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    val = arr[randint(0, len(arr) - 1)]
+    left = [i for i in arr if i < val]
+    center = [i for i in arr if i == val]
+    right = [i for i in arr if i > val]
+    return fast_sort(left) + center + fast_sort(right)
 
-        quickSort(arr, left, pivot-1)
-        quickSort(arr, pivot+1, right)
-
-    return arr
-
-def separation(arr, left, right):
-    pivot_index = random.randint(left, right)
-    arr[pivot_index], arr[right] = arr[right], arr[pivot_index]
-    pivot = arr[right]
-
-    real_left = left
-
-    for current in range (left, right):
-        if arr[current] < pivot:
-            arr[real_left], arr[current] = arr[current], arr[real_left]
-            real_left += 1
-
-    arr[real_left], arr[right] = arr[right], arr[real_left]
-    return real_left
 
 n = int(input())
-if n > 10**5:
-    sys.exit()
+arr = input().split()
+for i in range(n):
+    arr[i] = int(arr[i])
 
-arr = list(map(int, input().split()))
-
-for elem in arr:
-    if elem > 10**9 or elem < -10**9:
-        sys.exit()
-
-left = 0
-right = n-1
-
-arr = quickSort(arr, left, right)
-
-print(*arr,sep=' ')
+arr = quick_sort(arr)
+print(*arr)
